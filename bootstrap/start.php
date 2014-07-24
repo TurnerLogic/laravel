2da9c6dce8440e7182775b542a666034a7c26d33
cturner80 <chris@gizmovation.com>
2014-07-23 22:01:00 -0500

    Tweaked environment detection.

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -24,11 +24,11 @@ $app = new Illuminate\Foundation\Application;
 |
 */
 
-$env = $app->detectEnvironment(array(
+$env = $app->detectEnvironment(function () {
 
-	'local' => array('homestead'),
+	return isset($_SERVER['LARAVEL_ENV']) && $_SERVER['LARAVEL_ENV'] == 'local' ? 'local' : 'production';
 
-));
+});
 
 /*
 |--------------------------------------------------------------------------
@@ -54,8 +54,7 @@ $app->bindInstallPaths(require __DIR__.'/paths.php');
 |
 */
 
-$framework = $app['path.base'].
-                 '/vendor/laravel/framework/src';
+$framework = $app['path.base'].'/vendor/laravel/framework/src';
 
 require $framework.'/Illuminate/Foundation/start.php';
 
