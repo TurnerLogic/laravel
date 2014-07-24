2e0da1b18079ac5b9c1c5b418c8073c66fa2b22d
cturner80 <chris@gizmovation.com>
2014-07-23 23:02:46 -0500

    Added master layout and sample page.

--- gulpfile.js
+++ gulpfile.js
@@ -9,16 +9,24 @@ var rename = require('gulp-rename');
 var notify = require('gulp-notify');
 var es = require('event-stream');
 
+var bootswatchTheme = 'flatly';
+
 // copy bower dependencies to appropriate locations
 gulp.task('copy', function(cb) {
   return es.concat(
     gulp.src('bower_components/bootstrap/dist/fonts/*')
       .pipe(gulp.dest('public/fonts')),
-    gulp.src('bower_components/bootstrap/dist/css/bootstrap.min.css')
+    gulp.src('bower_components/bootswatch/'+bootswatchTheme+'/bootstrap.min.css')
       .pipe(gulp.dest('public/css')),
+    // gulp.src('bower_components/bootstrap/dist/css/bootstrap.min.css')
+    //   .pipe(gulp.dest('public/css')),
     gulp.src('bower_components/bootstrap/dist/js/bootstrap.min.js')
       .pipe(gulp.dest('public/js')),
     gulp.src('bower_components/jquery/dist/jquery.min.js')
+      .pipe(gulp.dest('public/js')),
+    gulp.src('bower_components/html5shiv/dist/html5shiv.min.js')
+      .pipe(gulp.dest('public/js')),
+    gulp.src('bower_components/respond/dest/respond.min.js')
       .pipe(gulp.dest('public/js'))
   );
 });
