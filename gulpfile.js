e3f3dc9f5992795a3498ea5ec9860263c8a25afd
cturner80 <chris@gizmovation.com>
2014-07-23 21:38:13 -0500

    Added gulp file and assets location.

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+// declare dependencies
+var gulp = require('gulp');
+var minifycss = require('gulp-minify-css');
+var autoprefixer = require('gulp-autoprefixer');
+var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var notify = require('gulp-notify');
+var es = require('event-stream');
+
+// copy bower dependencies to appropriate locations
+gulp.task('copy', function(cb) {
+  return es.concat(
+    gulp.src('bower_components/bootstrap/dist/fonts/*')
+      .pipe(gulp.dest('public/fonts')),
+    gulp.src('bower_components/bootstrap/dist/css/bootstrap.min.css')
+      .pipe(gulp.dest('public/css')),
+    gulp.src('bower_components/bootstrap/dist/js/bootstrap.min.js')
+      .pipe(gulp.dest('public/js')),
+    gulp.src('bower_components/jquery/dist/jquery.min.js')
+      .pipe(gulp.dest('public/js'))
+  );
+});
+
+// produce a concatenated, minified all css file
+gulp.task('css', function(cb) {
+  return gulp.src('assets/css/*.css')
+    .pipe(autoprefixer('last 1 version'))
+    .pipe(concat('all.min.css'))
+    .pipe(minifycss())
+    .pipe(gulp.dest('public/css'));
+});
+
+// produce a concatenated, uglified all js file
+gulp.task('js', function(cb){
+  return gulp.src('assets/js/*.js')
+    .pipe(concat('all.min.js'))
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(uglify())
+    .pipe(gulp.dest('public/js'));
+});
+
+// watch for changes
+gulp.task('watch', function() {
+  gulp.watch('assets/js/**', ['js']);
+  gulp.watch('assets/css/**', ['css']);
+});
+
+// define the default task list
+gulp.task('default', ['copy', 'css', 'js', 'watch']);
